1a278086fc4d76e39ad57ce50cc5d33e9d03175c
mohamed <mohamed_gsm@msn.com>
2021-05-23 15:52:17 +0200

    implement navigation componenet and navigation bottom and room database

---
+++ app/src/main/java/com/example/jobapp/activits/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.jobapp.activits
+
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.room.Room
+import com.example.jobapp.R
+import com.example.jobapp.databinding.ActivityMainBinding
+import com.example.jobapp.model.JobModel
+import com.example.jobapp.roomdatabase.AppDataBase
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       binding = DataBindingUtil.setContentView(this , R.layout.activity_main)
+
+
+        // Operation work fro fragment manger.
+        val navHostFragment : NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController   : NavController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setupWithNavController(navController)
+
+        // work tge action bar for fragment page.
+        val appBarConfiguration = AppBarConfiguration(setOf())
+        setupActionBarWithNavController(navController , appBarConfiguration)
+
+
+        //operation work hide and show action bar for fragment.
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id){
+
+                R.id.welcomeScreenFragment   -> supportActionBar!!.hide()
+
+                else -> supportActionBar!!.show()
+            }
+        }
+
+        // operation work hide and show navigation bottom with fragment.
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id){
+                R.id.homeScreenFragment -> binding.bottomNavigation.visibility = View.VISIBLE
+                R.id.searchFragment     -> binding.bottomNavigation.visibility = View.VISIBLE
+                R.id.favoriteFragment   -> binding.bottomNavigation.visibility = View.VISIBLE
+
+                else -> binding.bottomNavigation.visibility = View.INVISIBLE
+            }
+        }
+
+        var dataBase : AppDataBase = Room.databaseBuilder(this , AppDataBase::class.java , "job").allowMainThreadQueries().build()
+        dataBase.jobDao().insertFavoriteJob(JobModel("Programing"))
+    }
+}
---
+++ app/src/main/java/com/example/jobapp/welcomescreenfragment/WelcomeScreenFragment.kt
@@ -0,0 +1,19 @@
+package com.example.jobapp.welcomescreenfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.jobapp.R
+
+class WelcomeScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome_screen, container, false)
+    }
+}
